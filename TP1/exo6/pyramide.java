f90a805a9b41fe5dc569e7f445609b83e5e25b14
RubenPL0 <ruben.preti-_-levy@telecom-sudparis.eu>
2025-04-04 18:31:44 +0200

    Debut Tp2

--- TP1/exo6/pyramide.java
+++ TP1/exo6/pyramide.java
@@ -1,41 +1,41 @@
-//class Main {
+////class Main {
+////    public static void main(String[] args) {
+////        int var = 14;
+////        for (int i = 1; i <= var; i++) {
+////            for (int l = 1; l <= (var-i); l++) {
+////                System.out.print(" ");
+////            }
+////            for (int j = 1; j <= i; j++) {
+////                System.out.print("*");
+////            }
+////            for (int g = i - 1; g >= 1; g--) {
+////                System.out.print("*");
+////            }
+////            System.out.println();
+////        }
+////    }
+////}
+//
+//public class pyramide {
 //    public static void main(String[] args) {
-//        int var = 14;
-//        for (int i = 1; i <= var; i++) {
-//            for (int l = 1; l <= (var-i); l++) {
+//        int rows = 14; // Number of rows in the pyramid
+//
+//        // Loop over each row
+//        for (int i = 1; i <= rows; i++) {
+//            // Print leading spaces for current row
+//            for (int j = 1; j <= rows - i; j++) {
 //                System.out.print(" ");
 //            }
+//            // Print ascending numbers for current row
 //            for (int j = 1; j <= i; j++) {
-//                System.out.print("*");
+//                System.out.print(j);
 //            }
-//            for (int g = i - 1; g >= 1; g--) {
-//                System.out.print("*");
+//            // Print descending numbers for current row
+//            for (int j = i - 1; j >= 1; j--) {
+//                System.out.print(j);
 //            }
+//            // Move to the next line after completing the row
 //            System.out.println();
 //        }
 //    }
 //}
-
-public class pyramide {
-    public static void main(String[] args) {
-        int rows = 14; // Number of rows in the pyramid
-
-        // Loop over each row
-        for (int i = 1; i <= rows; i++) {
-            // Print leading spaces for current row
-            for (int j = 1; j <= rows - i; j++) {
-                System.out.print(" ");
-            }
-            // Print ascending numbers for current row
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
-            // Print descending numbers for current row
-            for (int j = i - 1; j >= 1; j--) {
-                System.out.print(j);
-            }
-            // Move to the next line after completing the row
-            System.out.println();
-        }
-    }
-}
